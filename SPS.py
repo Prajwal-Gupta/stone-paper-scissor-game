d0e1a2869fd43fb37e973ac2b6a5c9deb6ce3629
Prajwal-Gupta <61011807+Prajwal-Gupta@users.noreply.github.com>
2021-03-27 22:10:45 +0530

    Update SPS.py

--- SPS.py
+++ SPS.py
@@ -15,49 +15,20 @@ def click(event):
         newWindow.geometry("300x200")
         newWindow.resizable(0,0)
         Label(newWindow, text ="Computer selected: {}\nIts a tie :|\nTry again".format(b), font="cambria 15 bold").pack(expand=TRUE)
-    if a == 'Stone' and b == 'Paper':
-        
+
+    if (a == 'Stone' and b == 'Paper') or (a == 'Paper' and b == 'Scissor') or (a == 'Scissor' and b == 'Stone'):
         newWindow = Toplevel(root)
         newWindow.title("Result")
         newWindow.resizable(0,0)
         newWindow.geometry("300x200")
         Label(newWindow, text ="Computer selected: {}\nYou lost :(\nTry again".format(b), font="cambria 15 bold").pack(expand=TRUE)
-    if a == 'Stone' and b == 'Scissor':
-        
+
+    if (a == 'Stone' and b == 'Scissor') or (a == 'Paper' and b == 'Stone') or (a == 'Scissor' and b == 'Paper'):
         newWindow = Toplevel(root)
         newWindow.title("Result")
         newWindow.geometry("300x200")
         newWindow.resizable(0,0)
         Label(newWindow, text ="Computer selected: {}\nYou won :)\nTry again".format(b), font="cambria 15 bold").pack(expand=TRUE)
-    
-    if a == 'Paper' and b == 'Scissor':
-        
-        newWindow = Toplevel(root)
-        newWindow.title("Result")
-        newWindow.geometry("300x200")
-        newWindow.resizable(0,0)
-        Label(newWindow, text ="Computer selected: {}\nYou lost :(\nTry again".format(b), font="cambria 15 bold").pack(expand=TRUE)
-    if a == 'Paper' and b == 'Stone':
-        
-        newWindow = Toplevel(root)
-        newWindow.title("Result")
-        newWindow.geometry("300x200")
-        newWindow.resizable(0,0)
-        Label(newWindow, text ="Computer selected: {}\nYou won :)\nTry again".format(b), font="cambria 15 bold").pack(expand=TRUE)
-    if a == 'Scissor' and b == 'Paper':
-        
-        newWindow = Toplevel(root)
-        newWindow.title("Result")
-        newWindow.geometry("300x200")
-        newWindow.resizable(0,0)
-        Label(newWindow, text ="Computer selected: {}\nYou won :)\nTry again".format(b), font="cambria 15 bold").pack(expand=TRUE)
-    if a == 'Scissor' and b == 'Stone':
-        
-        newWindow = Toplevel(root)
-        newWindow.title("Result")
-        newWindow.geometry("300x200")
-        newWindow.resizable(0,0)
-        Label(newWindow, text ="Computer selected: {}\nYou lost :(\nTry again".format(b), font="cambria 15 bold").pack(expand=TRUE)
     
 root = Tk()
 root.geometry("500x300")
@@ -71,12 +42,12 @@ b1 = Button(f1,text = "Stone", height=1, width=10, font="cambria 15 bold")
 b2 = Button(f1,text = "Paper", height=1, width=10, font="cambria 15 bold")
 b3 = Button(f1,text = "Scissor", height=1, width=10, font="cambria 15 bold")
 qb = Button(f1, text = "Exit", height=1, width=10, font="cambria 15 bold")
+l.pack(pady=(0,10))
+
 qb.bind('<Button-1>', quit)
 b1.bind('<Button-1>', click)
 b2.bind('<Button-1>', click)
 b3.bind('<Button-1>', click)
-
-l.pack(pady=(0,10))
 
 b1.grid(row=1, column=0, padx=(10,0), pady=10)
 b2.grid(row=1, column=1, padx=(10,0), pady=10)
@@ -90,6 +61,7 @@ p3 = PhotoImage(file = 'scissor.png')
 lb1 = Label(f1, image = p1)
 lb2 = Label(f1, image = p2)
 lb3 = Label(f1, image = p3)
+
 lb1.grid(row=0, column=0, padx=(5,0), pady=5)
 lb2.grid(row=0, column=1, padx=(5,0), pady=5)
 lb3.grid(row=0, column=2, padx=(5,5), pady=5)
